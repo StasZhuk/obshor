fdcea87bd04e902411c818c26b1851cad8582723
Zhukovskiy Stanislav <menistrel2005@yandex.ru>
2017-08-21 23:33:42 +0300

    add adaptive menu + add page company obshor

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -4,5 +4,6 @@ module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
   './node_modules/svg4everybody/dist/svg4everybody.min.js',
   './node_modules/jquery-slimscroll/jquery.slimscroll.min.js',
-  './node_modules/magnific-popup/dist/jquery.magnific-popup.min.js'
+  './node_modules/magnific-popup/dist/jquery.magnific-popup.min.js',
+  // './node_modules/modernizr/src/Modernizr.js'
 ];
--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    (function($) {
+    (function ($) {
         $(".nav-slimscroll").slimScroll({
             height: '100vh',
             railOpacity: 0.5,
@@ -9,12 +9,42 @@ $(document).ready(function() {
             distance: '2px',
             color: '#000'
         })
-      })($);
+    })($);
 
-      $('.sertificats__gallery').magnificPopup({
+    $('.sertificats__gallery').magnificPopup({
         delegate: 'a', // child items selector, by clicking on it popup will open
         type: 'image',
-        gallery:{enabled:true}
+        gallery: {
+            enabled: true
+        }
         // other options
-      });
-})
+    });
+
+
+    //   nav trigger
+    // $('.nav__trigger').on('click', function() {
+    //     $('.nav-main').trigger('click');
+    // });
+    $(".nav__trigger").click(function() {
+        if ($(".nav__trigger").hasClass('active')) {
+                $(".nav__trigger").removeClass('active');
+                $(".nav__trigger").removeClass('no-animation');
+                $(".nav-main").slideToggle("0.5", "linear");
+        }
+        else {
+            $(".nav__trigger").addClass('active');
+            $(".nav__trigger").removeClass('no-animation');
+            $(".nav-main").slideToggle("0.5", "linear");
+            
+        }
+    });
+    $(window).resize(function() {
+        if($(window).width() >= 992) {
+            $(".nav-main").css('display', 'block'); 
+        }
+        else {
+            $(".nav-main").css('display', 'none'); 
+        }
+        return false;
+    });
+});
